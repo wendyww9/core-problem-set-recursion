e5120f616c6fd1e59fc9c8cacb00693d495058e5
wendyww9 <wendyskystar@gmail.com>
2025-05-01 20:20:50 -0700

    implemented recursive algorithm

--- part-1.py
+++ part-1.py
@@ -4,17 +4,37 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    
+    if n == 0:
+        return 1
+    return factorial(n-1) * n
 
 
 # reverse
-
+def reverse(text):
+    if text == "":
+        return ""
+    
+    return reverse(text[1:]) + text[0]
 
 
 # bunny
 
-
+def bunny(count):
+    if count == 0:
+        return 0
+        
+    return bunny(count-1) + 2
 
 # is_nested_parens
 
-
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+        
+    if len(parens) < 2 or parens[0] != '(' or parens[-1] != ')':
+        return False
+    return is_nested_parens(parens[1:-1])
--- part-2.py
+++ part-2.py
@@ -4,13 +4,43 @@
 # the appropriate comment.
 
 # search
-
+def search(array, query):
+    if array == []:
+        return False
+        
+    if array[0] == query:
+        return True
+        
+    return search(array[1:], query)
 
 
 # is_palindrome
-
+def is_palindrome(text):
+    if len(text) <= 1:
+        return True
+        
+    if text[0] != text[-1]:
+        return False
+        
+    return is_palindrome(text[1:-1])
 
 
 # digit_match
-
+def digit_match(apples, oranges):
+    
+    if apples == 0 or oranges == 0:
+        if apples % 10 == oranges % 10:
+            return 1 
+        else:
+            return 0
+        
+    if apples % 10 == oranges % 10:
+        match = 1 
+    else:
+        match = 0
+    
+    if apples < 10 or oranges < 10:
+        return match
+    
+    return match + digit_match(apples//10, oranges//10)
 
